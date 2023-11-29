82df539bdc2719eb4a772669f902e511c96e5cf5
omonsvetov <o.monsvetov@gmail.com>
2023-11-30 00:19:32 +0300

    well done but..

---
+++ components/DeleteButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { deleteDuty } from '@/app/actions';
+import Image from 'next/image';
+
+export default function DeleteButton({duty}: {duty:string}) {
+
+    return (
+        <button onClick={() => deleteDuty(duty)} >
+            <Image 
+                src="/images/delete.svg" 
+                width={20} 
+                height={20} 
+                alt="delete icon" 
+            />
+        </button>
+    );
+}
